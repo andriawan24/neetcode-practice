4c93923a7ae7b853c571280c71b61738c5f7202b
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-09-15 10:54:11 +0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,6 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev2, prev1 = 1, 1
+        for i in range(2, n + 1):
+            prev2, prev1 = prev1, prev2 + prev1
+        return prev1
